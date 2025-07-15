891f29d0e1cf09a328908e35a9475b8758ab9bcd
simon montfort <simon.montfort@graduateinstitute.ch>
2025-07-15 11:08:34 +0200

    - Version working on the KUMA HPC of EPFL - added mixed precision for speed-up - removed user-specific directories

--- script/job.sh
+++ script/job.sh
@@ -5,15 +5,16 @@
 #SBATCH --time=24:00:00
 #SBATCH --cpus-per-task=20
 #SBATCH --mem=256G
-#SBATCH --partition=l40s
+#SBATCH --partition=h100
 #SBATCH --gres=gpu:1
 
-# Load the one that works, e.g.:
-module purge
-module load gcc cuda cudnn openmpi py-tensorflow
+export BASE_DIR="/home/smontfor/cities-learning-dec"
 
-# Run your script
-export TOKENIZERS_PARALLELISM=false
-nvidia-smi              
-# python -u 01_DEC_training.py
-python -u test_gpu.py
+module purge
+module load gcc cuda # cudnn # python openmpi py-tensorflow
+
+# Manually source Conda's shell hook
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate tf2.14
+
+python -u script/01_DEC_training.py
